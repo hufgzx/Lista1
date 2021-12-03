2fb65c897a45b57ad9a99cf2d0bc4da8552c5613
Gabi Duartte <66841994+hufgzx@users.noreply.github.com>
2021-12-02 22:57:20 -0300

    Update q5_somaelementos.js

--- q5_somaelementos.js
+++ q5_somaelementos.js
@@ -6,8 +6,8 @@ const n = Number(input('Digite um numero com 3 digitos: '))
 // Processamento
 cent = n/100
 r1 = n%100
-dez = r/10
-uni = r%10
+dez = r1/10
+uni = r1%10
 rtot = cent + dez + uni
 
 // Saída
